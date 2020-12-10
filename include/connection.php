3f0373f3254ac17cd8cbfb2d57e40f8655d8dffd
suberkhazi <suberkhazi@gmail.com>
2020-12-10 16:17:56 +0530

    index file created

--- include/connection.php
+++ include/connection.php
@@ -6,10 +6,10 @@ $password = "";
 $db_name = "olms";
 
 $con = new mysqli($server, $username, $password, $db_name);
-if(!$con) {
-    die ("connection failed:" . mysqli_connect_error);
-}
-else {
-    echo "<script> alert('successfully connected') </script>";
-}
+#if(!$con) {
+ #   die ("connection failed:" . mysqli_connect_error);
+#}
+#else {
+#    echo "<script> alert('successfully connected') </script>";
+#}
 ?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JCET Library management system</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body>
+<div class="col-md-12">
+            <div class="heading">
+                <h1>Library Management System</h1>
+            </div>
+        </div>
+<div id="allTheThings">
+			<div id="member">
+				<a href="student/index.php">
+					<img src="dist/images/landing/ic_member.svg" width="250px" height="auto"/><br />
+					<h3>Student</h3>
+				</a>
+			</div>
+			<div id="verticalLine">
+				<div id="librarian">
+					<a id="librarian-link" href="librarian/index.php">
+						<img src="dist/images/landing/ic_librarian.svg" width="250px" height="auto" /><br />
+						<h3>Librarian</h3>
+					</a>
+				</div>
+			</div>
+		</div>
+ 
+</body>
+</html>

316cb99302bbe7886dcb0b2ffd7da0749bb0957b
suberkhazi <suberkhazi@gmail.com>
2020-12-11 10:30:05 +0530

    new comments

--- include/connection.php
+++ include/connection.php
@@ -6,10 +6,14 @@ $password = "";
 $db_name = "olms";
 
 $con = new mysqli($server, $username, $password, $db_name);
-#if(!$con) {
- #   die ("connection failed:" . mysqli_connect_error);
-#}
-#else {
-#    echo "<script> alert('successfully connected') </script>";
-#}
+
+/* to check the connection
+if(!$con) {
+   die ("connection failed:" . mysqli_connect_error);
+}
+else {
+    echo "<script> alert('successfully connected') </script>";
+}
+*/
+
 ?>
